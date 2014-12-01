ee3aeee174e6201bb1587f890adaf5623db8a5d0
Ivan Uemlianin <ivan@llaisdy.com>
2014-12-01 16:43:31 +0000

    liga calibration progress

--- calibration/data_server.erl
+++ calibration/data_server.erl
@@ -1,8 +1,11 @@
 -module(data_server).
 -behaviour(gen_server).
 
--export([start_link/0,
+-compile([export_all]).
+
+-export([start_link/0, stop/0,
 	 size/1, size/2,
+	 get_labels/0, get_accounts/1,
 	 get_with_complement/3
 	]).
 
@@ -20,6 +23,7 @@
 		   {nl, "nl_NL"}
 		  ]).
 
+%% data = dict {lang : {acct : [str]}}
 -record(state, {data}).
 
 %%%% API
@@ -27,29 +31,86 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+stop() ->
+    gen_server:cast(?SERVER, stop).
+
 size(Lab) ->
     gen_server:call(?SERVER, {size, Lab}).
 
 size(Lab, Acc) ->
     gen_server:call(?SERVER, {size, Lab, Acc}).
 
-get_with_complement(Lab, N1, N2) ->
-    gen_server:call(?SERVER, {get_with_complement, Lab, N1, N2}).
+get_labels() ->
+    gen_server:call(?SERVER, get_labels).
+
+get_accounts(Lab) ->
+    gen_server:call(?SERVER, {get_accounts, Lab}).
+
+get_with_complement(Lab, NGet, NComp) ->
+    gen_server:call(?SERVER, {get_with_complement, Lab, NGet, NComp}).
 
 %%%% gen_server callbacks
 
 init([]) ->
     Data = lists:foldl(fun({Lab, Dir}, Dict) ->
 			       LabDir = ?DATA_DIR ++ "/" ++ Dir,
-			       import_data(Dict, LabDir)
+			       dict:store(Lab, import_data(LabDir), Dict)
 		       end,
 		       dict:new(),
 		       ?LAB_DIRS),
     {ok, #state{data=Data}}.
 
+handle_call(get_labels, _From, State=#state{data=Data}) ->
+    Reply = dict:fetch_keys(Data),
+    {reply, Reply, State};
+
+handle_call({get_accounts, Lab}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		dict:fetch_keys(D)
+	end,
+    E = [],
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+
+handle_call({size, Lab}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		dict:fold(fun(_, V, A) -> 
+				  A + length(V)
+			  end,
+			  0, D)
+	end,
+    E = 0,
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+			       
+handle_call({size, Lab, Acc}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		case dict:find(Acc, D) of
+		    error -> 0;
+		    {ok, AL} -> length(AL)
+		end
+	end,
+    E = 0,
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+
+handle_call({get_with_complement, Lab, NGet, NComp}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		All = dict:fold(fun(_, V, A) -> V ++ A end, [], D),
+		Ngc = NGet + NComp,
+		{Sel,_} = lists:split(Ngc, [X||{_,X} <- lists:sort([{random:uniform(), N} || N <- All])]),
+		lists:split(NGet, Sel)
+	end,
+    E =  {[],[]},
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -67,12 +128,20 @@ code_change(_OldVsn, State, _Extra) ->
 
 % fold_files(Dir, RegExp, Recursive, Fun, AccIn)
 
--spec import_data(dict(), string()) -> dict().
-import_data(Dict, Dir) ->
+-spec import_data(string()) -> dict:dict().
+import_data(Dir) ->
     filelib:fold_files(Dir, "\\d+_\\d+.txt", false,
 		       fun(Fn, D) ->
-			       Ac = string:sub_word(Fn, 1, $_),
-			       todo, % read file, bin to list (utf8)
-			       dict:do(D, Ac, 
+			       Ac = string:sub_word(string:sub_word(Fn, 3, $/), 1, $_),
+			       {ok, B} = file:read_file(Fn),
+			       Str = unicode:characters_to_list(B, utf8),
+			       dict:append(Ac, Str, D)
 		       end, 
-		       Dict).
+		       dict:new()).
+
+do_if_valid_key(Lab, Data, F, Else) ->
+    case dict:find(Lab, Data) of
+	error -> Else;
+	{ok, LD} -> F(LD)
+    end.
+
