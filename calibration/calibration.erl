c2a8789813d925f0f4b42eaa636b51ad5cc83a41
Ivan Uemlianin <ivan@llaisdy.com>
2014-12-04 13:35:52 +0000

    liga calibration more tromp tests

--- calibration/calibration.erl
+++ calibration/calibration.erl
@@ -3,8 +3,6 @@
 
 -type labelled_string() :: {atom(), string()}.
 
--define(LABELS, [de, en, es, fr, it, nl]).
-
 %%%% For each setup, do fifty different ten-fold validation tests & take average.
 
 calibrate() ->
@@ -33,38 +31,87 @@ do_trials(N, {NTestsPerTrial, Type, Set}) ->
      do_trials(N - 1, {NTestsPerTrial, Type, Set})].
 
 do_tests(Type, Set, N) ->
-    {TrainingData, TestData} = setup(Type, Set, N),
-    {Type, Set, run(TrainingData, TestData)}.
+    Frames = setup(Type, Set, N),
+    run_frames(Frames).
 
-
-%%%% training data: random x% of each language
-%%%% test data: random ten from rest (all six languages)
 
 -spec setup(atom(), atom(), non_neg_integer()) -> {[labelled_string()], [labelled_string()]}.
 setup(sample_size, PCage, NTests) ->
+    %% for each language:
+    %% training data: random x% of each language
+    %% test data: random NTests from rest
+    Labs = data_server:get_labels(),
     Xs = lists:map(fun(Lab) ->
-			  Size = data_server:size(Lab),
-			  NTrain = Size * PCage div 100,
-			  data_server:get_with_complement(Lab, NTrain, NTests)
+			   data_server:get_with_complement(Lab, PCage, NTests)
+		   end,
+		   Labs),
+    [{{sample_size, PCage}, 
+      lists:foldl(fun({A, B}, {C, D}) ->
+			  {A++C, B++D}
 		  end,
-		  ?LABELS),
-    lists:foldl(fun({A, B}, {C, D}) ->
-			{A++C, B++D}
-		end,
-	       {[],[]},
-	       Xs);
+		  {[],[]},
+		  Xs)}];
+
+setup(spec_gen, spec, NTests) ->
+    %% for each language:
+    %% training data = 2/3 of all data of one single account
+    %% test data 1 (specialisation) = all rest of data from that account
+    Labs = data_server:get_labels(),
+    Xs = lists:map(fun(Lab) ->
+			   Acc = data_server:get_account(Lab),
+			   data_server:get_with_complement(Lab, Acc, 67, NTests)
+		   end,
+		   Labs),
+    [{spec,
+      lists:foldl(fun({A, B}, {C, D}) ->
+			  {A++C, B++D}
+		  end,
+		  {[],[]},
+		  Xs)}];
+
+setup(spec_gen, gen, NTests) ->
+    %% for each language:
+    %% training data = 2/3 of all data of one single account
+    %% test data 2 (generalisation) = data from all other accounts
+    Labs = data_server:get_labels(),
+    Xs = lists:map(fun(Lab) ->
+			   [Acc | Rest] = data_server:shuffle_accounts(Lab),
+			   TrainingData = data_server:get_data(Lab, Acc, 67),
+			   TestData = lists:foldl(fun(Ra, Ac) ->
+							  data_server:get_data(Lab, Ra, NTests) ++ Ac
+						  end,
+						  [],
+						  Rest),
+			   {TrainingData, TestData}
+			   
+		   end,
+		   Labs),
+    [{gen,
+      lists:foldl(fun({A, B}, {C, D}) ->
+			  {A++C, B++D}
+		  end,
+		  {[],[]},
+		  Xs)}];
+    
+
 
 setup(Type, Set, _) ->
     {e_not_implemented, Type, Set}.
 
--spec run([labelled_string()], [labelled_string()]) -> any().
-run(TrainingData, TestData) ->
+run_frames([]) -> [];
+run_frames([H|T]) -> 
+    [run_one(H) | run_frames(T)].
+
+-spec run_one({tuple(), {[labelled_string()], [labelled_string()]}}) -> any().
+run_one({TestLabel, {TrainingData, TestData}}) ->
     M = build_model(TrainingData),
-    lists:map(fun({Lab, Str}) ->
-		      Res = liga:classify(M, Str),
-		      {correct(Res, Lab), Res}
-	      end,
-	      TestData).
+    R = lists:map(fun({Lab, Str}) ->
+			  Res = liga:classify(M, Str),
+			  {Lab, Res}
+			  %% {correct(Res, Lab), Res}
+		  end,
+		  TestData),
+    {TestLabel, R}.
 
 build_model(Ls) ->
     lists:foldl(fun({Label, String}, Model) ->
--- calibration/data_server.erl
+++ calibration/data_server.erl
@@ -1,12 +1,18 @@
 -module(data_server).
 -behaviour(gen_server).
-
 -compile([export_all]).
+
+-type labelled_string() :: {atom(), string()}.
 
 -export([start_link/0, stop/0,
 	 size/1, size/2,
-	 get_labels/0, get_accounts/1,
-	 get_with_complement/3
+	 get_labels/0,
+	 get_accounts/1, shuffle_accounts/1,
+	 get_account/1,
+	 get_label_account/0,
+	 get_data/3,
+	 get_with_complement/3,
+	 get_with_complement/4
 	]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -46,8 +52,31 @@ get_labels() ->
 get_accounts(Lab) ->
     gen_server:call(?SERVER, {get_accounts, Lab}).
 
-get_with_complement(Lab, NGet, NComp) ->
-    gen_server:call(?SERVER, {get_with_complement, Lab, NGet, NComp}).
+shuffle_accounts(Lab) ->
+    Accs = get_accounts(Lab),
+    shuffle(Accs).
+
+get_account(Lab) ->
+    choose_one(get_accounts(Lab)).
+
+get_label_account() ->
+    Lab = choose_one(get_labels()),
+    Acc = choose_one(get_accounts(Lab)),
+    {Lab, Acc}.
+
+-spec get_with_complement(atom(), pcage(), non_neg_integer() | all) 
+			 -> {[labelled_string()],[labelled_string()]}.
+get_with_complement(Lab, PCage, NComp) ->
+    gen_server:call(?SERVER, {get_with_complement, Lab, PCage, NComp}).
+
+-type pcage() :: non_neg_integer().
+-spec get_with_complement(atom(), any(), pcage(), non_neg_integer() | all) 
+			 -> {[labelled_string()],[labelled_string()]}.
+get_with_complement(Lab, Acc, PCage, NComp) ->
+    gen_server:call(?SERVER, {get_with_complement, Lab, Acc, PCage, NComp}).
+
+get_data(Lab, Acc, N) ->
+    gen_server:call(?SERVER, {get_data, Lab, Acc, N}).
 
 %%%% gen_server callbacks
 
@@ -74,10 +103,7 @@ handle_call({get_accounts, Lab}, _From, State=#state{data=Data}) ->
 
 handle_call({size, Lab}, _From, State=#state{data=Data}) ->
     F = fun(D) ->
-		dict:fold(fun(_, V, A) -> 
-				  A + length(V)
-			  end,
-			  0, D)
+		get_size(D)
 	end,
     E = 0,
     Reply = do_if_valid_key(Lab, Data, F, E),
@@ -94,12 +120,54 @@ handle_call({size, Lab, Acc}, _From, State=#state{data=Data}) ->
     Reply = do_if_valid_key(Lab, Data, F, E),
     {reply, Reply, State};
 
-handle_call({get_with_complement, Lab, NGet, NComp}, _From, State=#state{data=Data}) ->
+handle_call({get_with_complement, Lab, PCage, NComp}, _From, State=#state{data=Data}) ->
     F = fun(D) ->
 		All = dict:fold(fun(_, V, A) -> V ++ A end, [], D),
-		Ngc = NGet + NComp,
+		Size = length(All),
+		NGet = Size * PCage div 100,
+		Ngc = case NComp of 
+			  all -> Size;
+			  _   -> NGet + NComp
+		      end,
 		{Sel,_} = lists:split(Ngc, [X||{_,X} <- lists:sort([{random:uniform(), {Lab,N}} || N <- All])]),
 		lists:split(NGet, Sel)
+	end,
+    E =  {[],[]},
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+
+handle_call({get_with_complement, Lab, Acc, PCage, NComp}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		All = case dict:find(Acc, D) of
+			  error -> [];
+			  {ok, AL} -> AL
+		      end,
+		Size = length(All),
+		NGet = Size * PCage div 100,
+		Ngc = case NComp of 
+			  all -> Size;
+			  _   -> NGet + NComp
+		      end,
+		{Sel,_} = lists:split(Ngc, [X||{_,X} <- lists:sort([{random:uniform(), {Lab,N}} || N <- All])]),
+		lists:split(NGet, Sel)
+	end,
+    E =  {[],[]},
+    Reply = do_if_valid_key(Lab, Data, F, E),
+    {reply, Reply, State};
+
+handle_call({get_data, Lab, Acc, NGet}, _From, State=#state{data=Data}) ->
+    F = fun(D) ->
+		All = case dict:find(Acc, D) of
+			  error -> [];
+			  {ok, AL} -> AL
+		      end,
+		Size = length(All),
+		Ng = case NGet > Size of
+			 true -> Size;
+			 false -> NGet
+		     end,
+		{Sel,_} = lists:split(Ng, [X||{_,X} <- lists:sort([{random:uniform(), {Lab,N}} || N <- All])]),
+		Sel
 	end,
     E =  {[],[]},
     Reply = do_if_valid_key(Lab, Data, F, E),
@@ -126,7 +194,20 @@ code_change(_OldVsn, State, _Extra) ->
 
 %%%% Private
 
-% fold_files(Dir, RegExp, Recursive, Fun, AccIn)
+choose_one(L) ->
+    lists:nth(random:uniform(length(L)), L).
+
+do_if_valid_key(Lab, Data, F, Else) ->
+    case dict:find(Lab, Data) of
+	error -> Else;
+	{ok, LD} -> F(LD)
+    end.
+
+get_size(D) ->
+    dict:fold(fun(_, V, A) -> 
+		      A + length(V)
+	      end,
+	      0, D).
 
 -spec import_data(string()) -> dict:dict().
 import_data(Dir) ->
@@ -139,9 +220,5 @@ import_data(Dir) ->
 		       end, 
 		       dict:new()).
 
-do_if_valid_key(Lab, Data, F, Else) ->
-    case dict:find(Lab, Data) of
-	error -> Else;
-	{ok, LD} -> F(LD)
-    end.
-
+shuffle(L) ->
+    [X||{_,X} <- lists:sort([{random:uniform(), N} || N <- L])].
