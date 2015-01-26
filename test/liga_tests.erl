9d02d53503acdf009a45d2a2092f9cafbc6692b1
Ivan Uemlianin <ivan@llaisdy.com>
2015-01-26 16:31:06 +0000

    ready for publication

--- test/liga_tests.erl
+++ test/liga_tests.erl
@@ -4,15 +4,7 @@
 
 build_model_small_test_() ->
     Exp = liga_test_utils:get_model_small(),
-    Act = lists:foldl(fun({String, Label}, Model) ->
-			      liga:import_string(Model, 
-						 String, 
-						 Label)
-		      end,
-		      liga:new(),
-		      [{"is dit een test", nl},
-		       {"is this a test", en}]
-		     ),
+    Act = build_model_small(),
     ?_assertEqual(Exp, Act).
 
 classify_small_test_() ->
@@ -20,4 +12,13 @@ classify_small_test_() ->
     Exp = [{nl,0.5744047619047619}, {en,0.14583333333333331}],
     Act = liga:classify(M, "is dit ook een test"),
     ?_assertEqual(Exp, Act).
+
+build_model_small() ->
+    lists:foldl(fun({String, Label}, Model) ->
+			liga:import_string(Model, String, Label)
+		end,
+		liga:new(),
+		[{"is dit een test", nl},
+		 {"is this a test", en}]
+	       ).
 
